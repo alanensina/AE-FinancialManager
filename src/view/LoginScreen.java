4d2ee2213062d9eba3ab45e0bd816aadc51aef70
Alan Ensina <alanvinicius.ce@gmail.com>
2019-01-05 19:40:54 -0200

    Adjusting the Login Screen

--- src/view/LoginScreen.java
+++ src/view/LoginScreen.java
@@ -10,12 +10,14 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
+import java.awt.Font;
 
 public class LoginScreen extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
+	private final JLabel lblCopyrightAlan = new JLabel("Alan Ensina @ 2019");
 
 	/**
 	 * Launch the application.
@@ -40,7 +42,7 @@ public class LoginScreen extends JFrame {
 		setResizable(false);
 		setTitle("AE Financial Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 248, 328);
+		setBounds(100, 100, 365, 308);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,38 +50,42 @@ public class LoginScreen extends JFrame {
 		
 		JLabel lblLogin = new JLabel("Username");
 		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLogin.setBounds(12, 159, 81, 15);
+		lblLogin.setBounds(54, 165, 81, 15);
 		contentPane.add(lblLogin);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPassword.setBounds(12, 186, 81, 15);
+		lblPassword.setBounds(54, 192, 81, 15);
 		contentPane.add(lblPassword);
 		
-		textField = new JTextField();
-		textField.setBounds(100, 157, 120, 19);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtUsername = new JTextField();
+		txtUsername.setBounds(142, 163, 173, 19);
+		contentPane.add(txtUsername);
+		txtUsername.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(100, 184, 120, 19);
-		contentPane.add(passwordField);
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(142, 190, 173, 19);
+		contentPane.add(txtPassword);
 		
-		JButton btnSingIn = new JButton("Sing in");
-		btnSingIn.setBounds(22, 221, 93, 25);
-		contentPane.add(btnSingIn);
+		JButton btSingIn = new JButton("Sing in");
+		btSingIn.setBounds(12, 221, 108, 25);
+		contentPane.add(btSingIn);
 		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(127, 221, 93, 25);
-		contentPane.add(btnCancel);
+		JButton btCancel = new JButton("Cancel");
+		btCancel.setBounds(237, 221, 108, 25);
+		contentPane.add(btCancel);
 		
-		JButton btnNewUser = new JButton("New user?");
-		btnNewUser.setBounds(62, 258, 117, 25);
-		contentPane.add(btnNewUser);
+		JButton btNewUser = new JButton("New user?");
+		btNewUser.setBounds(125, 221, 108, 25);
+		contentPane.add(btNewUser);
 		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(LoginScreen.class.getResource("/icons/money-bag (1).png")));
-		lblLogo.setBounds(22, 12, 198, 135);
+		lblLogo.setBounds(22, 12, 323, 135);
 		contentPane.add(lblLogo);
+		lblCopyrightAlan.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCopyrightAlan.setFont(new Font("Dialog", Font.PLAIN, 8));
+		lblCopyrightAlan.setBounds(12, 248, 333, 33);
+		contentPane.add(lblCopyrightAlan);
 	}
 }
